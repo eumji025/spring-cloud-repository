ea9c09745cd95cf9e3ca3569f1de663c09439a59
eumji025 <zombie_goo@yeah.net>
2017-04-30 16:18:29 +0800

    spring cloud 外部配置的client

---
+++ spring-cloud-config-client/src/main/java/com/eumji/config/SpringCloudConfigClientApplication.java
@@ -0,0 +1,20 @@
+package com.eumji.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCloudConfigClientApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+	}
+}
