0ed56b4f2509a8e62518eb2c9863736323779756
eumji025 <zombie_goo@yeah.net>
2017-05-06 22:33:47 +0800

    update 相应的文字

--- spring-cloud-discovery-eureka-client/src/main/java/com/eumji/discovery/SpringCloudDiscoveryEurekaClientApplication.java
+++ spring-cloud-discovery-eureka-client/src/main/java/com/eumji/discovery/SpringCloudDiscoveryEurekaClientApplication.java
@@ -2,9 +2,7 @@ package com.eumji.discovery;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +19,7 @@ public class SpringCloudDiscoveryEurekaClientApplication {
 	@RequestMapping("/")
 	public String home() {
 		logger.info("hello world,this is eureka client1");
-		return "Hello World";
+		return "hello world,this is eureka client1";
 	}
 
 	public static void main(String[] args) {
--- spring-cloud-discovery-eureka-client2/src/main/java/com/eumji/discovery/SpringCloudDiscoveryEurekaClientApplication.java
+++ spring-cloud-discovery-eureka-client2/src/main/java/com/eumji/discovery/SpringCloudDiscoveryEurekaClientApplication.java
@@ -2,9 +2,7 @@ package com.eumji.discovery;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +20,7 @@ public class SpringCloudDiscoveryEurekaClientApplication {
 	@RequestMapping("/")
 	public String home() {
 		logger.info("hello world,this is eureka client2");
-		return "Hello World";
+		return "hello world,this is eureka client2";
 	}
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(SpringCloudDiscoveryEurekaClientApplication.class).web(true).run(args);
