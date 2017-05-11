6386ac35ccbdf3d22a7b8ec89e9ffc61adfa2556
eumji025 <zombie_goo@yeah.net>
2017-05-11 10:51:06 +0800

    使用springbootapplication注解并且加上后面需要使用的route功能的代理方法

--- spring-cloud-discovery-eureka-client/src/main/java/com/eumji/discovery/SpringCloudDiscoveryEurekaClientApplication.java
+++ spring-cloud-discovery-eureka-client/src/main/java/com/eumji/discovery/SpringCloudDiscoveryEurekaClientApplication.java
@@ -3,6 +3,7 @@ package com.eumji.discovery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class SpringCloudDiscoveryEurekaClientApplication {
@@ -20,6 +20,24 @@ public class SpringCloudDiscoveryEurekaClientApplication {
 	public String home() {
 		logger.info("hello world,this is eureka client1");
 		return "hello world,this is eureka client1";
+	}
+
+	/**
+	 * zuul proxy first demo
+	 * @return
+	 */
+	@RequestMapping("/proxy/add")
+	public String addUser(){
+		return "add user success,  this proxy by zuul";
+	}
+
+	/**
+	 * zuul proxy first demo2
+	 * @return
+	 */
+	@RequestMapping("/add")
+	public String addUser2(){
+		return "add user success,  this proxy by zuul";
 	}
 
 	public static void main(String[] args) {
