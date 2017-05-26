2572f5645c5bde2bc10c6a00fbf02b30d98ac9d3
eumji <zombie_goo@yeah.net>
2017-05-26 23:16:53 +0800

    记录消息的例子

---
+++ spring-cloud-stream-transform/src/main/java/com/eumji/stream/SpringCloudStreamTransformApplication.java
@@ -0,0 +1,12 @@
+package com.eumji.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamTransformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamTransformApplication.class, args);
+	}
+}
---
+++ spring-cloud-stream-transform/src/test/java/com/eumji/stream/SpringCloudStreamTransformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eumji.stream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudStreamTransformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
