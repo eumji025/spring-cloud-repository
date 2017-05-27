809a5f7603e213d8d797728ec8a942203fb212d5
eumji <zombie_goo@yeah.net>
2017-05-27 17:48:41 +0800

    对sink进行微调

--- spring-cloud-stream-binding-input/src/main/java/com/eumji/stream/SinkDemo.java
+++ spring-cloud-stream-binding-input/src/main/java/com/eumji/stream/SinkDemo.java
@@ -3,8 +3,8 @@ package com.eumji.stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.integration.annotation.ServiceActivator;
 
 /**
  * Created by eumji on 17-5-25.
@@ -13,10 +13,8 @@ import org.springframework.integration.annotation.ServiceActivator;
 public class SinkDemo {
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @ServiceActivator(inputChannel = Sink.INPUT)
+    @StreamListener(Sink.INPUT)
     public void sinkMessage(Object message){
-
         logger.info("received message: "+message);
-        System.out.println("received message: "+message);
     }
 }
