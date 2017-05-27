dddcd5bc0dc42d1fae768a1ded463c644a380ae6
eumji <zombie_goo@yeah.net>
2017-05-27 17:50:07 +0800

    对transform模块进行微调，去除无用的代码

--- spring-cloud-stream-transform/src/main/java/com/eumji/stream/LoggingTransformer.java
+++ spring-cloud-stream-transform/src/main/java/com/eumji/stream/LoggingTransformer.java
@@ -2,7 +2,6 @@ package com.eumji.stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.integration.annotation.ServiceActivator;
@@ -12,20 +11,12 @@ import org.springframework.integration.annotation.ServiceActivator;
  * Created by eumji on 17-5-26.
  */
 @EnableBinding(Processor.class)
-@ConfigurationProperties("module.logging")
 public class LoggingTransformer {
 
     private static Logger logger = LoggerFactory.getLogger(LoggingTransformer.class);
 
-    private String name = "logging";
+    private static String name = "logging";
 
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @ServiceActivator(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
     public Object transform(Object payload) {
