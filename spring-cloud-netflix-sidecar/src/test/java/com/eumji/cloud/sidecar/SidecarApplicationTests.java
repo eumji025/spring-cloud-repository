fed19df4c980fdeb258acb493d5f90e4721f63d6
eumji025 <eumji025@gmail.com>
2017-10-30 16:31:07 +0800

    新增Sidecar通过zuul获取所有服务的实例信息

---
+++ spring-cloud-netflix-sidecar/src/test/java/com/eumji/cloud/sidecar/SidecarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eumji.cloud.sidecar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SidecarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
