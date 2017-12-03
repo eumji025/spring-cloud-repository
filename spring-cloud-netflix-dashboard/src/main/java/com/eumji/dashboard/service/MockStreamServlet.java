c7a400274f9696137835304fe87e8e38c9a6402e
EumJI <eumji025@gmail.com>
2017-12-03 09:48:52 +0800

    修复一些项目结构的问题

--- spring-cloud-netflix-dashboard/src/main/java/com/eumji/dashboard/service/MockStreamServlet.java
+++ spring-cloud-netflix-dashboard/src/main/java/com/eumji/dashboard/service/MockStreamServlet.java
@@ -23,6 +23,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.nio.charset.Charset;
 
 /**
@@ -43,6 +44,7 @@ public class MockStreamServlet extends HttpServlet {
     /**
      * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
      */
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String filename = request.getParameter("file");
         if (filename == null) {
